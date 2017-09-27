3019b4cf9303a66bb8fc995cd1946787c8b591ef
michellejkwon <michellejkwon@gmail.com>
2017-09-27 16:34:31 -0700

    completed hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(baz)
+    @baz = baz
+  end
+
+  def bar(arg1, hash = {})
+    arg1.to_s + @baz + hash[:sat].to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  get '/', to: 'pages#home'
   get 'age', to: 'pages#age'
+  put 'age', to: 'pages#person'
+  post '/', to: 'pages#stringify', as: 'stringify'
+
+  get 'me', to: 'pages#me'
 end
